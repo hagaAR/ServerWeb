acc27632c251dcfe8f80cd7747e9e03d90786850
Haga Radaod <haga.radaody@gmail.com>
2015-02-06 14:28:37 +0100

    Init commit

---
+++ ServerWeb.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var fs = require('fs');
+
+var serverWeb = http.createServer(function(req,res){
+	fs.readFile('./Client.html', 'utf-8', function(error, content) {
+		res.writeHead(200, {"Content-Type": "text/html"});
+		res.end(content); 
+	});
+});
+serverWeb.listen(8080);
