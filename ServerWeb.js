d3f34af96bde79ffbd67198813da70969e425148
Haga Radaod <haga.radaody@gmail.com>
2015-02-09 09:22:11 +0100

    Creation db JSON

--- ServerWeb.js
+++ ServerWeb.js
@@ -5,7 +5,6 @@ var serverWeb = http.createServer(function(req,res){
 	fs.readFile('./Client.html', 'utf-8', function(error, content) {
 		res.writeHead(200, {"Content-Type": "text/html"});
 		res.end(content); 
-		
 	});
 });
 serverWeb.listen(8080);
