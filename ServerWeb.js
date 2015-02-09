e9067a1d2f42e7f7245889f37c8471254d05a6c3
Haga Radaod <haga.radaody@gmail.com>
2015-02-09 10:52:02 +0100

    Creation Statut

--- ServerWeb.js
+++ ServerWeb.js
@@ -7,4 +7,16 @@ var serverWeb = http.createServer(function(req,res){
 		res.end(content); 
 	});
 });
+
+// Chargement de socket.io
+var io = require('socket.io').listen(serverWeb);
+// Quand on client se connecte, on le note dans la console
+io.sockets.on('connection', function (socket) {
+	console.log('Un client est connecté !'); 
+	socket.on('statut_alice',function (statut) {
+		console.log('Un client envoie son statut ! Son ID est : ' + statut.statut_alice.i_d); 
+	});
+});
+
+
 serverWeb.listen(8080);
