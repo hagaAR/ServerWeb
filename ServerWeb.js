581cc13464324cee882bc6627b7f9dfb994d9a44
Haga Radaod <haga.radaody@gmail.com>
2015-02-17 07:34:39 +0100

    Ajout de la prise de vidéo
    prise de video en locale + geolocalisation partielle (envoye au serveur
    mais débile) + manque connexion P2P multimedia (en gros du délire) +
    manque interface graphique

--- ServerWeb.js
+++ ServerWeb.js
@@ -24,27 +24,64 @@ io.sockets.on('connection', function (socket) {
 			WebID: socket.id
 		};
 		listAssoPeerWebID.push(AssoPeerWebID);
+		//envoie de tout le monde (mais pas de sous liste)
 		socket.emit('envoieDB',listDiff);
 		//ajout ds la bdd des clients
 		listDiff.push(statut);
 		//emission de cette bdd a tous les autres
 		socket.broadcast.emit('envoieDB',listDiff);
-		console.log(listDiff[0].nom);
+		//console.log(listDiff[0].nom);
 	});
+	//gestion de la deconnexion
 	socket.on('disconnect',function(){
 		var k=0;
 		//dissociation de PeerServer et WebServer??
-		var indicemax= listDiff.length-1;
+		var indicemax= listDiff.length;
+		//console.log('client '+ socket.id +' se deconnecte');
 		if (listDiff.length<0){indicemax=0;}
+		console.log('indice max: '+indicemax);
 		while (k<indicemax){
 			if (listAssoPeerWebID[k].WebID==socket.id){
+				console.log('client '+ socket.id +' se deconnecte');
 				//enlever cet objet du tableau listDiff : http://jonathankowalski.fr/blog/2011/12/supprimer-un-element-dans-un-tableau-javascript/
+				listDiff.splice(k,1);
+				listAssoPeerWebID.splice(k,1);
+				console.log('client'+ socket.id +' est enleve des listes');
 				//enlever cet objet de listAssoPeerWebID 
 				socket.broadcast.emit('envoieDB',listDiff); //renvoyer la DB aux clients
 				break;
 			}
 			k++
 		};
+		socket.broadcast.emit('envoieDB',listDiff);
+	});
+
+	//gestion de MAJ des statuts
+	socket.on('maj_statut',function(statut){
+		var k=0;
+		//dissociation de PeerServer et WebServer??
+		var indicemax= listDiff.length;
+		//console.log('client '+ socket.id +' se deconnecte');
+		if (listDiff.length<0){indicemax=0;}
+		console.log('indice max: '+indicemax);
+		while (k<indicemax){
+			if (listAssoPeerWebID[k].WebID==socket.id){
+				console.log('client '+ socket.id +' se MAJ');
+				//enlever cet objet du tableau listDiff : http://jonathankowalski.fr/blog/2011/12/supprimer-un-element-dans-un-tableau-javascript/
+				listDiff[k]=statut;
+				socket.broadcast.emit('envoieDB',listDiff); //renvoyer la DB aux clients
+				break;
+			}
+			k++
+		};
+		socket.broadcast.emit('envoieDB',listDiff);
+	});
+
+	//gestion des demandes des localisations
+	socket.on('demandelocalisations',function(coordonnees){
+		//recherche sur google en placant tout le monde dans la requete https://developers.google.com/maps/
+		//et renvoie d'une sous liste
+		socket.broadcast.emit('envoieDB',sous_listDiff);
 	});
 });
 
